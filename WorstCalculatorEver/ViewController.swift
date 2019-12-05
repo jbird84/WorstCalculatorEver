779d15b78276d4ad609a5c0ee4b2f0a644e7e6b0
Josh Kinney <joshkinney@kinneykare.com>
2019-12-04 21:12:08 -0600

    Updated some of the code to clean up a bit

--- WorstCalculatorEver/ViewController.swift
+++ WorstCalculatorEver/ViewController.swift
@@ -9,12 +9,62 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var firstNumberTextField: UITextField!
+    @IBOutlet weak var secondNumberTextField: UITextField!
+    @IBOutlet weak var AnswerTextField: UITextField!
+    
+    var result: Double = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    
+    @IBAction func additionPressed(_ sender: Any) {
+        if let firstNum = Double(firstNumberTextField.text!) {
+            if let secondNum = Double(secondNumberTextField.text!) {
+                result = firstNum + secondNum
+                AnswerTextField.text = String(result)
+            }
+        }
+    }
+    
+    
+    
+    @IBAction func subtractPressed(_ sender: Any) {
+        if let firstNum = Double(firstNumberTextField.text!) {
+            if let secondNum = Double(secondNumberTextField.text!) {
+                result = firstNum - secondNum
+                AnswerTextField.text = String(result)
+            }
+        }
+    }
+    
+    
+    
+    @IBAction func multiplyPressed(_ sender: Any) {
+        if let firstNum = Double(firstNumberTextField.text!) {
+            if let secondNum = Double(secondNumberTextField.text!) {
+                result = firstNum * secondNum
+                AnswerTextField.text = String(result)
+            }
+        }
+    }
+    
+    
+    
+    @IBAction func dividePressed(_ sender: Any) {
+        if let firstNum = Double(firstNumberTextField.text!) {
+            if let secondNum = Double(secondNumberTextField.text!) {
+                result = firstNum / secondNum
+                AnswerTextField.text = String(result)
+            }
+        }
+    }
+    
+    
+    
 }
 
